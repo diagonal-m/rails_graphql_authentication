d7957032e1d5cee69fb0da54b1b9e68dcc9868b5
佐藤太一 <3101taichi@gmail.com>
2022-07-02 16:42:31 +0900

    クエリ操作prac

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -8,10 +8,11 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :test_field, String, null: false, description: "An example field added by the generator" do
+      argument :name, String, required: true
+    end
+    def test_field(name:)
+      "Hello #{name}!"
     end
   end
 end
